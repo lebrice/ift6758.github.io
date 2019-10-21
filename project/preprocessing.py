c5839cda1ffb9ac368911c7c4008643b86cbed9c
MarieStLaurent <marie.stl@gmail.com>
2019-10-21 13:05:48 -0400

    Temporary commit while fixing branch setup
    Signed-off-by: MarieStLaurent <marie.stl@gmail.com>

--- project/preprocessing.py
+++ project/preprocessing.py
@@ -1,56 +1,132 @@
 import os
 import glob
-import tensorflow as tf
-import tensorflow_hub as hub
-from typing import *
 
-import image_preprocessing
-import text_preprocessing
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+import tensorflow as tf
+
+from typing import *
+from collections import Counter
+
 import likes_preprocessing
 
-def get_user_ids(input_dir: str) -> List[str]:
-    """Given an input_dir, returns the list of userids. This is to be used as the reference, in terms of ordering, for all other datasets.
+# notes: merging in pandas
+# https://pandas.pydata.org/pandas-docs/stable/user_guide/merging.html
 
-    Arguments:
-        input_dir {str} -- The parent input directory (ex, "~/Train"), should have a "Image" subfolder with image files.
-
-    Returns:
-        List[str] -- [description]
+def get_text_data(input_dir):
     """
-    image_files_name_pattern = os.path.join(input_dir, "Image", "*.jpg")
-    image_filepaths = glob.glob(image_files_name_pattern)
-    file_names = (os.path.basename(file_path) for file_path in image_filepaths)
-    userids = [file_name.split(".")[0] for file_name in file_names]
-    userids = sorted(userids)
-    return userids
-
-
-def make_dataset(input_dir: str) -> tf.data.Dataset:
-    """Creates the main dataset
-
-    Arguments:
-        input_dir {str} -- The parent input directory (for example, "~/Train").
-
-    Returns:
-        tf.data.Dataset -- A `tf.data.Dataset` containing the userids and the processed versions of the images (feature vector), the text (feature vector) and the likes (one-hot binary vector). 
+    Input
+        input_dir {string} : path to input_directory (ex, "~/Train")
+    Output:
+        id_list {numpy array of strings}: array of ids (to order feature and label matrices)
+        liwc_scaler {fitted sklearn MinMaxScaler}: fitted scaler to scale liwc test
+                                                    data similarly to the train data
+        text_data {pandas DataFrame of float}: normalized text data (liwc and nrc)
     """
-    preprocessing_batch_size = 100
-    userids = get_user_ids(input_dir)
-    
-    userids_dataset = tf.data.Dataset.from_tensor_slices(userids)
-    preprocessed_images = image_preprocessing.make_dataset(input_dir, userids)
-    # preprocessed_texts = text_preprocessing.make_dataset(input_dir, userids)
-    # preprocessed_likes = likes_preprocessing.make_dataset(input_dir, userids)
-    
-    return tf.data.Dataset.zip({
-        "userids": userids_dataset,
-        "image_in": preprocessed_images,
-        # "text_in": preprocessed_texts,
-        # "likes_in": preprocessed_likes,
-    })
+    # Build list of subject ids
+    liwc = pd.read_csv(os.path.join(input_dir, 'Text/liwc.csv'), sep = ',')
+    liwc = liwc.sort_values(by=['userId'])
 
-if __name__ == "__main__":
-    ds = make_dataset("./debug_data")
-    for i, features in enumerate(ds):
-        print(features)
-    print(ds)
+    nrc = pd.read_csv(os.path.join(input_dir, 'Text/nrc.csv'), sep = ',')
+    nrc = nrc.sort_values(by=['userId'])
+
+    # check if same subject lists in both sorted DataFrames (liwc and nrc)
+    if np.array_equal(liwc['userId'], nrc['userId']):
+        id_list = liwc['userId'].to_numpy()
+    else:
+        # returns numpy arrays of sorted unique subject ids
+        liwc_list = np.sort(liwc['userId'].unique(), axis=None)
+        nrc_list = np.sort(nrc['userId'].unique(), axis=None)
+
+        id_list = np.union1d(liwc_list, nrc_list)
+        '''
+        TO DO: re-order liwc and nrc matrices to match id_list order
+        '''
+        # code here...
+    '''
+    normalize liwc data (range between 0 and 85100) by squishing between 0 and 1
+    Note: nrc data is already between 0 and 1
+    https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.MinMaxScaler.html#sklearn.preprocessing.MinMaxScaler
+    https://medium.com/@urvashilluniya/why-data-normalization-is-necessary-for-machine-learning-models-681b65a05029
+    '''
+    liwc.set_index('userId', inplace=True)
+    liwc_scaler = preprocessing.MinMaxScaler()
+    liwc_scaler = liwc_scaler.fit(liwc)
+    liwc_scaled_vals = liwc_scaler.transform(liwc) #Note: returns numpy array, not pd.DataFrame
+    liwc_scaled = pd.DataFrame(liwc_scaled_vals, index = liwc.index, columns=liwc.columns)
+
+    # merge liwc and nrc dataframes
+    nrc.set_index('userId', inplace=True)
+    # surprise data scaled from 0-0.5: double its sort_value
+    nrc['surprise'] = nrc['surprise']*2
+    text_data = pd.concat([liwc_scaled, nrc], axis=1, sort=False)
+
+    return id_list, liwc_scaler, text_data
+
+def get_image_dataset(data_dir, sub_ids):
+    # Features extracted from Image (face metrics)
+    # 7915 entries (9500 ids), some without faces, a few people with multiple faces.
+    oxford = pd.read_csv(os.path.join(input_dir, 'Image/oxford.csv'), sep = ',')
+    # list of ids with at least one entry
+    # TO DO: determine which to chose
+    ox_list = np.sort(oxford['userId'].unique(), axis=None)
+    # list of ids with multiple faces in one picture
+    # TO DO: create two additional features (1 hot) : 1 if multiple faces, 1 if no face
+    ox_multiples = oxford['userId'][oxford['userId'].duplicated()] # 741
+    # list of ids in text_list who have no face metrics in oxford
+    ox_noface = np.setdiff1d(text_list, ox_list)
+
+    return image_data
+
+def preprocess_train(data_dir, return_scaler=True):
+    '''
+    Input
+        data_dir {string}: path to Train data directory
+        return_scaler {boolean}: if True, function returns liwc scaler
+    Output:
+        x_train {pandas DataFrame}: train set  vectorized features
+        y_train {pandas DataFrame}: train set labels
+        liwc_scaler {fitted sklearn MinMaxScaler}: fitted scaler to scale liwc test
+                                                    data similarly to the train data
+
+    TO DO: convert outputted pandas to tensorflow tf.data.Dataset...
+    https://www.tensorflow.org/guide/data
+    '''
+    # get numpy array of subject ids,
+    # a scaler object to scale the liwc test data,
+    # and a pandas DataFrame of preprocessed text data (liwc and nrc)
+    sub_ids, liwc_scaler, text_data = get_text_data(data_dir)
+
+    # get pandas dataframe of oxford data
+    image_data = get_image_dataset(data_dir, sub_ids)
+
+    if return_scaler:
+        return x_train, y_train, liwc_scaler
+    else:
+        return x_train, y_train
+
+def get_train_val_sets(data_dir):
+    '''
+    Input
+        data_dir {string}: path to Train data directory
+    Output:
+        x_train, x_val {pandas DataFrames}: train and validation sets vectorized features
+        y_train, y_val {pandas DataFrames}: train and validation set labels
+
+    TO DO: convert outputted pandas to tensorflow tf.data.Dataset?...
+    https://www.tensorflow.org/guide/data
+    '''
+    X_data, Y_data = preprocess_train(data_dir, False)
+
+
+def preprocess_test(data_dir, liwc_scaler):
+    '''
+    Input:
+        datadir {string}: path to Test data directory
+        liwc_scaler {sklearn MinMaxScaler}: to scale the liwc test data just like the train data
+        the function returns a DataFrame of labels (default = False)
+    Output:
+        x_test {pandas DataFrame}: test set  vectorized features
+    '''
+    return
--- project/text_preprocessing.py
+++ project/text_preprocessing.py
@@ -1,45 +1,28 @@
+import pandas as pd
+import numpy as np
+
 import tensorflow as tf
-import tensorflow_hub as hub
 from typing import *
 
 
-def get_text_dataset(input_dir: str, userids: List[str]) -> tf.data.Dataset:
-    """Returns a tf.data.Dataset of the text for each user in `userids`.
-    
-    Arguments:
-        input_dir {str} -- The parent input directory.
-        userids {List[str]} -- the list of userids. the output dataset should have the same ordering as this list.
-    
-    Returns:
-        tf.data.Dataset -- a tf.data.Dataset of strings #* Fab: Preferably a tf.data.Dataset. Might also work with pandas directly, I'm not sure.
+def make_dataset(input_dir, userids):
     """
-    raise NotImplementedError() # TODO
+    Creates DataFrame of preprocessed text dataset (1 row per userid).
 
+    Input:
+    input_dir -- the parent input directory (e.g. ~/Train)
+    userids -- numpy arrays of unique userids
 
-
-def get_text_preprocessing_fn() -> Callable:
-    # embedding models: These are pre-trained feature extractors available from tensorflow-hub:
-    text_embedding_module_url = "https://tfhub.dev/google/tf2-preview/nnlm-en-dim128/1"
-    embed_text = hub.KerasLayer(text_embedding_module_url, output_shape=[128], input_shape=[], dtype=tf.string, trainable=False, name="text_features")
-    @tf.function
-    def preprocess_text(text):
-        return embed_text
-    
-    return preprocess_text
-
-
-def make_dataset(input_dir: str, userids: List[str]) -> tf.data.Dataset:
-    """Creates the preprocessed text dataset for the given userid's.
-    
-    Arguments:
-        input_dir {str} -- the parent input directory
-        userids {List[str]} -- the list of userids
-    
-    Returns:
-        tf.data.Dataset -- the preprocessed text dataset, where each entry is the feature vector.
+    Output:
+    text_dataset -- pandas DataFrame of preprocessed text data (liwc and nrc).
     """
+    liwc = pd.read_csv(os.path.join(input_dir, 'Text/liwc.csv'), sep = ',')
+    liwc_list = np.sort(liwc['userId'].unique(), axis=None) # returns numpy array
+    nrc = pd.read_csv(os.path.join(input_dir, 'Text/nrc.csv'), sep = ',')
+    nrc_list = np.sort(nrc['userId'].unique(), axis=None) # returns numpy array
+
     userid_dataset = tf.data.Dataset.from_tensor_slices(userids)
-    
+
     text_preprocessing_fn = get_text_preprocessing_fn()
     preprocessing_batch_size = 100
 
