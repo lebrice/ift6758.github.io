d21e2f7ea6672d9235ac143c73ed789605d58c27
Fabrice Normandin <fabrice.normandin@gmail.com>
2019-11-03 17:33:54 -0500

    Starting Experiment with Loss Weights
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- project/hyperparameter_tuning.sh
+++ project/hyperparameter_tuning.sh
@@ -4,7 +4,7 @@ pip install --quiet orion
 
 # TODO: change this to the maximum number of desired trials.
 MAX_TRIALS=20
-EXPERIMENT_NAME="slim_models"
+EXPERIMENT_NAME="changing_loss_weights"
 
 mkdir -f logs
 
@@ -15,13 +15,15 @@ orion -v --debug hunt --max-trials $MAX_TRIALS -n $EXPERIMENT_NAME ./ift6758.git
         --epochs $MAX_EPOCHS_PER_EXPERIMENT \
         --batch_size 64 \
         --num_layers~"randint(1, 3)" \
-        --dense_units~"choices(32, 64, 128)" \
+        --dense_units~"choices(32, 64)" \
         --activation tanh \
         --learning_rate~"choices(0.01, 0.005)" \
         --num_like_pages 5000 \
         --use_dropout True \
         --use_batchnorm False \
         --optimizer sgd \
-        --l1_reg~"choices(0.01, 0.005)" \
-        --l2_reg~"choices(0.01, 0.005)" \
+        --l1_reg 0.005 \
+        --l2_reg 0.005 \
+        --gender_loss_weight~"randint(1, 10)" \
+        --age_loss_weight~"randint(1, 10)" \
         >> "logs/$EXPERIMENT_NAME.txt"
