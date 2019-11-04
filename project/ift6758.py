887eea0221e147f30d9aff55d874716ade212218
remidion <remi.dion@gmail.com>
2019-11-04 18:10:17 +0000

    added threshold, TODO: modify input paths

--- project/ift6758.py
+++ project/ift6758.py
@@ -60,21 +60,49 @@ class average_user :
     
     def getGender:
         #TODO PATH_IMAGE + PATH_PROFILES
-        profiles = pd.read_csv(PATH_PROFILES)
-        oxford = pd.read_csv(PATH_IMAGES)
+        #profiles = pd.read_csv(PATH_PROFILES)
         
+        #profiles = pd.read_csv(PATH_PROFILES)
+        liwc = pd.read_csv(PATH_LIWC)
+        nrc = pd.read_csv(PATH_NRC)
+        likes = pd.read_csv(PATH_LIKES)
+        oxford = pd.read_csv(PATH_IMAGES)
+
+
+        liwc = liwc.rename(columns={"userId":"userid"})
+        oxford = oxford.rename(columns={"userId":"userid"})
+        nrc = nrc.rename(columns={"userId":"userid"})
+        likes = likes.rename(columns={"userId":"userid"})        
+
+        user_ids = liwc.merge(oxford["userid"], on="userid", how='outer')
+        user_ids = user_ids.merge(nrc["userid"], on="userid", how='outer')
+        user_ids = user_ids.merge(likes["userid"], on="userid", how='outer')
+        user_ids=user_ids.loc[:,'userid'].unique()
+
         oxford = oxford.rename(columns={"userId":"userid"})
         oxford.drop_duplicates(subset ="userid",keep = "first", inplace=True)
-        
+
         facial_hair=oxford.loc[:,['userid']]
         facial_hair['hair']= oxford.facialHair_sideburns+oxford.facialHair_mustache+oxford.facialHair_beard
-        
+
         t_gender = profiles.merge(facial_hair, on="userid")
         t_gender.set_index(keys='userid', inplace=True, drop=True)
         t_gender.drop(['Unnamed: 0'], axis=1).head()
-        
+
         Male=t_gender[['hair']][t_gender.loc[:,'gender'] == 0]
         Female=t_gender[['hair']][t_gender.loc[:,'gender'] == 1]
+
+        #women = pd.DataFrame([facial_hair['hair']<0.25], columns='gender')
+
+        women=facial_hair.loc[:,['userid']]
+        #women.set_index('userid')
+        #facial_hair.set_index('userid')
+        women['genre']=facial_hair['hair']<0.25
+
+        #gender=user_ids.merge(women, on="userid", how='outer')
+
+        #print(women)
+        return women
 
 
 def main():
