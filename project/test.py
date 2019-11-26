aa8efeaeb55c4fd635368bfdb8b69bc3c2d2528d
Fabrice Normandin <fabrice.normandin@gmail.com>
2019-11-26 16:04:29 -0500

    Fixed Test Preprocessing bug
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- project/test.py
+++ project/test.py
@@ -51,48 +51,18 @@ class TestConfig:
             self.image_means_train = to_np_float_array(f.readline())
         
         self.test_features = preprocess_test(self.i, self.min_max_train, self.image_means_train, self.likes_kept_train)
-        self.test_features.drop(["noface", "multiface"], axis=1, inplace=True)
 
 
 
 
 
 
-def test_input_pipeline(data_dir: str, test_config: TestConfig):
+def test_input_pipeline(data_dir: str, test_config: TestConfig, hparams: HyperParameters):
     test_features = test_config.test_features
 
     # TODO: save the information that will be used in the testing phase to a file or something.
     column_names = list(test_features.columns)
     print("number of columns:", len(column_names))
-    train_columns = [
-        'WC', 'WPS', 'Sixltr', 'Dic', 'Numerals', 'funct', 'pronoun', 'ppron',
-        'i', 'we', 'you', 'shehe', 'they', 'ipron', 'article', 'verb', 'auxverb',
-        'past', 'present', 'future', 'adverb', 'preps', 'conj', 'negate', 'quant',
-        'number', 'swear', 'social', 'family', 'friend', 'humans', 'affect', 'posemo',
-        'negemo', 'anx', 'anger', 'sad', 'cogmech', 'insight', 'cause', 'discrep',
-        'tentat', 'certain', 'inhib', 'incl', 'excl', 'percept', 'see', 'hear',
-        'feel', 'bio', 'body', 'health', 'sexual', 'ingest', 'relativ', 'motion',
-        'space', 'time', 'work', 'achieve', 'leisure', 'home', 'money', 'relig',
-        'death', 'assent', 'nonfl', 'filler', 'Period', 'Comma', 'Colon', 'SemiC',
-        'QMark', 'Exclam', 'Dash', 'Quote', 'Apostro', 'Parenth', 'OtherP', 'AllPct',
-        'positive', 'negative', 'anger', 'anticipation', 'disgust', 'fear', 'joy',
-        'sadness', 'surprise', 'trust', 'faceRectangle_width', 'faceRectangle_height',
-        'faceRectangle_left', 'faceRectangle_top', 'pupilLeft_x', 'pupilLeft_y', 'pupilRight_x',
-        'pupilRight_y', 'noseTip_x', 'noseTip_y', 'mouthLeft_x', 'mouthLeft_y', 'mouthRight_x',
-        'mouthRight_y', 'eyebrowLeftOuter_x', 'eyebrowLeftOuter_y', 'eyebrowLeftInner_x',
-        'eyebrowLeftInner_y', 'eyeLeftOuter_x', 'eyeLeftOuter_y', 'eyeLeftTop_x', 'eyeLeftTop_y',
-        'eyeLeftBottom_x', 'eyeLeftBottom_y', 'eyeLeftInner_x', 'eyeLeftInner_y', 'eyebrowRightInner_x',
-        'eyebrowRightInner_y', 'eyebrowRightOuter_x', 'eyebrowRightOuter_y', 'eyeRightInner_x',
-        'eyeRightInner_y', 'eyeRightTop_x', 'eyeRightTop_y', 'eyeRightBottom_x', 'eyeRightBottom_y',
-        'eyeRightOuter_x', 'eyeRightOuter_y', 'noseRootLeft_x', 'noseRootLeft_y', 'noseRootRight_x',
-        'noseRootRight_y', 'noseLeftAlarTop_x', 'noseLeftAlarTop_y', 'noseRightAlarTop_x',
-        'noseRightAlarTop_y', 'noseLeftAlarOutTip_x', 'noseLeftAlarOutTip_y', 'noseRightAlarOutTip_x',
-        'noseRightAlarOutTip_y', 'upperLipTop_x', 'upperLipTop_y', 'upperLipBottom_x', 'upperLipBottom_y',
-        'underLipTop_x', 'underLipTop_y', 'underLipBottom_x', 'underLipBottom_y', 'facialHair_mustache',
-        'facialHair_beard', 'facialHair_sideburns', 'headPose_roll', 'headPose_yaw',
-        # the like columns aren't here, obviously.
-    ]
-
 
     assert "faceID" not in column_names
     assert "userId" not in column_names
@@ -101,17 +71,13 @@ def test_input_pipeline(data_dir: str, test_config: TestConfig):
 
     # message = f"columnds present in train set but not in test set: {set(train_columns) ^ set(column_names)}"
     # assert len(column_names) == expected_num_columns, message
-    image_features_start_index=column_names.index("faceRectangle_width")
-    likes_features_start_index=column_names.index("headPose_yaw") + 1
 
     all_features=test_features.values
-    text_features=all_features[..., : image_features_start_index]
-    image_features=all_features[..., image_features_start_index: likes_features_start_index]
-    likes_features = all_features[..., likes_features_start_index:]
+    text_features, image_features, likes_features = split_features(all_features, hparams)
     
-    # print(text_features.shape, text_features.dtype)
-    # print(image_features.shape, image_features.dtype)
-    # print(likes_features.shape, likes_features.dtype)
+    print(text_features.shape, text_features.dtype)
+    print(image_features.shape, image_features.dtype)
+    print(likes_features.shape, likes_features.dtype)
     features_dataset= tf.data.Dataset.from_tensor_slices(
         {
             "userid": test_features.index.astype(str),
@@ -122,6 +88,30 @@ def test_input_pipeline(data_dir: str, test_config: TestConfig):
     )
     return features_dataset.batch(hparams.batch_size)
 
+def split_features(features: np.ndarray, hparams: HyperParameters) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """Split the ndarray with the three types of features into three ndarrays.
+    
+    Args:
+        features (np.ndarray): [description]
+    
+    Returns:
+        Tuple[np.ndarray, np.ndarray, np.ndarray]: [description]
+    """
+    
+    text_features_start_index = 0
+    text_features_end_index = text_features_start_index + hparams.num_text_features
+
+    image_features_start_index = text_features_end_index
+    image_features_end_index = image_features_start_index + hparams.num_image_features
+    
+    likes_features_start_index = image_features_end_index
+    likes_features_end_index = likes_features_start_index + hparams.num_like_pages 
+
+    text_features   = features[..., text_features_start_index:text_features_end_index]
+    image_features  = features[..., image_features_start_index:image_features_end_index]
+    likes_features  = features[..., likes_features_start_index:likes_features_end_index]
+
+    return text_features, image_features, likes_features
 
 if __name__ == "__main__":
     parser=argparse.ArgumentParser()
@@ -149,8 +139,7 @@ if __name__ == "__main__":
     )
 
     trained_model_weights_path=os.path.join(trained_model_dir, "model.h5")
-    trained_model_hparams_path=os.path.join(
-        trained_model_dir, "hyperparameters.json")
+    trained_model_hparams_path=os.path.join(trained_model_dir, "hyperparameters.json")
     trained_model_config_path=os.path.join(trained_model_dir, "train_config.json")
     
     import json
@@ -158,13 +147,17 @@ if __name__ == "__main__":
 
     with open(trained_model_hparams_path) as f:
         hparams=HyperParameters(**json.load(f))
+        print("Hyperparameters:", hparams)
+        print("number of text features:", hparams.num_text_features)
+        print("number of image features:", hparams.num_image_features)
+        print("number of like features:", hparams.num_like_pages)
 
     with open(trained_model_config_path) as f:
         train_config=TrainConfig(**json.load(f))
 
     model=get_model(hparams)
     model.load_weights(trained_model_weights_path)
-    test_dataset=test_input_pipeline(input_dir, test_config)
+    test_dataset=test_input_pipeline(input_dir, test_config, hparams)
 
     pred_labels = ["age_group", "gender", "ext", "ope", "agr", "neu", "con"]
 
