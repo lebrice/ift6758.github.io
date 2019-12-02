d7375e6c491549a8878a468c4a7decd370ae8d3f
Remi D <remi.dion@gmail.com>
2019-12-02 14:43:38 -0500

    tuning.sh -> Branch=New_Rel

--- project/hyperparameter_tuning.sh
+++ project/hyperparameter_tuning.sh
@@ -2,7 +2,7 @@
 
 pip install --quiet orion
 
-EXPERIMENT_NAME="one-model-each-marie-3"
+EXPERIMENT_NAME="New_Rel"
 
 latest_tag=`git --git-dir=ift6758.github.io/.git --work-tree=ift6758.github.io describe --tags`
 echo "latest tag is '$latest_tag'"
@@ -20,7 +20,7 @@ mkdir -p logs
 # TODO: change this to the maximum number of desired trials.
 MAX_TRIALS=100
 MAX_EPOCHS_PER_EXPERIMENT=500
-orion -v --debug hunt --max-trials $MAX_TRIALS -n $EXPERIMENT_NAME ./ift6758.github.io/project/train.py \
+orion -v --debug hunt --max-trials $MAX_TRIALS -n $EXPERIMENT_NAME ./train.py \
         --experiment_name $EXPERIMENT_NAME \
         --epochs $MAX_EPOCHS_PER_EXPERIMENT \
         --batch_size~"choices(64, 128, 256)" \
